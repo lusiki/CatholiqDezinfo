5cd01874f49341c5b2441720e847687cff37ef0d
BrbanMiro <luka.sikic.gm@gmail.com>
2023-12-04 16:21:33 +0100

    weiter

--- Application/app.R
+++ Application/app.R
@@ -4,7 +4,7 @@ library(shinythemes)
 # Define UI with an improved aesthetic
 ui <- fluidPage(
   theme = shinytheme("yeti"), # Using a shinythemes theme for better aesthetics
-  titlePanel("Detekcija dezinformacija katoličke tematike"),
+  titlePanel("Kritička analiza dezinformacija o vjerskim temama."),
   fluidRow(
     column(12,
            textAreaInput("text", "Unesite tekst objave:", rows = 10, placeholder = "Ovdje unesite tekst..."),
@@ -13,7 +13,14 @@ ui <- fluidPage(
   ),
   hr(), # Horizontal line for better separation of content
   h4("Rezultati analize:"),
-  textOutput("result")
+  uiOutput("result"),
+
+  # Footer
+  tags$footer(
+    style = "position: fixed; bottom: 0; width: 100%; background-color: #f5f5f5; text-align: center; padding: 7px;",
+    "© 2023 Projekt u suradnji Hrvatskog katoličkog sveučilišta, Hrvatskog katoličkog radija i Hrvatskog društva katoličkih novinara za uspostavu sustava provjere točnosti informacija.“ | ",
+    a(href = "https://www.unicath.hr/projekt-kriticka-analiza-dezinformacija-o-vjerskim-temama-hks-a-i-partnerskih-institucija-dobio-bespovratna-sredstva-za-uspostavu-sustava-provjere-tocnosti-informacija"," Web")
+  )
 )
 
 # Define server logic
@@ -47,27 +54,30 @@ server <- function(input, output) {
       )
 
       # Formatting the results
-      output$result <- renderText({
-        result_text <- ""
+      output$result <- renderUI({
+        result_text <- HTML("") # Initialize as HTML
 
         if (!is.null(results$KatoličkaTema)) {
-          result_text <- paste(result_text, "Tekst objave odgovara katoličkoj tematici. Identificirane riječi su:", results$KatoličkaTema, "\n\n")
+          katolicka_tema_text <- paste("Tekst objave odgovara katoličkoj tematici.<br>Identificirane riječi su:", results$KatoličkaTema, "<br><br>")
+          result_text <- HTML(paste(result_text, katolicka_tema_text, sep = ""))
         }
 
-        result_text <- paste(result_text, "Identificirana područja katoličke tematike:\n\n")
-
+        areas_text <- "Identificirana područja katoličke tematike:<br><br>"
         for (area in c("Fraze", "Pravno", "Politika", "Institucije")) {
           if (!is.null(results[[area]])) {
-            result_text <- paste(result_text, sprintf("%s: %s\n\n", area, results[[area]]))
+            area_text <- paste(sprintf("%s:<br>%s<br><br>", area, results[[area]]))
+            areas_text <- paste(areas_text, area_text, sep = "")
           }
         }
 
-        if (trimws(result_text) == "Identificirana područja katoličke tematike:") {
-          result_text <- "Nisu pronađene riječi koje upučuju na dezinformacije."
+        if (areas_text == "Identificirana područja katoličke tematike:<br><br>") {
+          areas_text <- "Nisu pronađene riječi koje upučuju na dezinformacije."
         }
 
+        result_text <- HTML(paste(result_text, areas_text, sep = ""))
         return(result_text)
       })
+
     } else {
       output$result <- renderText("Unesite tekst za provjeru.")
     }
