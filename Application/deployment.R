33ea34bf5647640160269d0164c3e2e2f168c4e2
BrbanMiro <luka.sikic.gm@gmail.com>
2023-12-04 13:29:28 +0100

    weiter

--- Application/deployment.R
+++
@@ -1,6 +0,0 @@
-rsconnect::setAccountInfo(name='luxsikic',
-  token='D9DCEF3D319EB61BFB76962AE5E81403',
-  secret='Bbld1bOg8+N93WodT46+Xx7v+iZ33TmLsB7dC20f')
-
-
-rsconnect::deployApp('../Application/app.R')
