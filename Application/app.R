18ce6fa40c827a1930ea0e28dcdf008649bc6b5f
BrbanMiro <luka.sikic.gm@gmail.com>
2023-12-05 15:51:18 +0100

    weiter

--- Application/app.R
+++ Application/app.R
@@ -1,35 +1,82 @@
+
+
+#libs----
 library(shiny)
 library(shinythemes)
+library(shinycssloaders)
+library(tidyverse)
+library(shiny)
+library(shinythemes)
+library(shinycssloaders)
+library(tidyverse)
+library(data.table)
+library(readxl)
+library(xlsx)
+library(here)
+library(kableExtra)
+library(DT)
+library(purrr)
+library(data.table)
+library(tidytext)
+library(dplyr)
+library(lubridate)
+library(anytime)
+library(grid)
+library(wordcloud)
+library(reshape2)
+library(ggraph)
+library(widyr)
+library(topicmodels)
+library(ggthemes)
+library(xlsx)
+library(writexl)
+library(data.table)
+library(stringi)
+library(openxlsx)
+
+#source----
+#
+#
+# source("./helper_files/stemmer.R")
+# source("./helper_files/text_analysis.R")
 
 # Define UI with an improved aesthetic
 ui <- fluidPage(
   theme = shinytheme("yeti"), # Using a shinythemes theme for better aesthetics
 
   tags$header(
-    style = "background-color: #f5f5f5; padding: 6px; text-align: center;",
-    tags$img(src = "logo.jpg", height = "50px"),
-    tags$h2("Kritička analiza dezinformacija o vjerskim temama", style = "color: #333;"),
-    tags$h4("HRVATSKO KATOLIČKO SVEUČILIŠTE", style = "color: #555;")
+    style = "background-color: #f5f5f5; padding-top: 2px; padding-bottom: 6px; text-align: center;",
+    tags$img(src = "logo.jpg", height = "30px"),
+    tags$h2("Kritička analiza dezinformacija o vjerskim temama", style = "color: #333; margin-top: 5px;"),
+    tags$h4("", style = "color: #555;")
   ),
 
-
-
-  titlePanel(""),
-  fluidRow(
-    column(12,
-           textAreaInput("text", "", rows = 10, placeholder = "Ovdje unesite tekst..."),
-           actionButton("check", "Analiziraj", class = "btn-primary")
+  tabsetPanel(
+    tabPanel("Unos podataka", # The existing content
+             fluidRow(
+               column(12, # This sets the width to the full available width
+                      textAreaInput("text", "", rows = 10, placeholder = "Ovdje unesite tekst..."),
+                      actionButton("check", "Analiziraj", class = "btn-primary"),
+                      textOutput("text_feedback") %>% withSpinner() # Adding the spinner here
+               )
+             )
+    ),
+    tabPanel("Analiza", # Placeholder for Analysis tab
+             h2("")
+    ),
+    tabPanel("Opis", # About tab content
+             h2(""),
+             p("Projekt „Kritička analiza dezinformacija o vjerskim temama“ Hrvatskog katoličkog sveučilišta te njegovih partnera Hrvatskog katoličkog radija i Hrvatskog društva katoličkih novinara prošao je na javnom pozivu za dodjelu bespovratnih sredstava za uspostavu sustava provjere točnosti informacija u okviru mjere „Uspostava provjere medijskih činjenica i sustava javne objave podataka“. „Uz samu provjeru dezinformacija izradit ćemo alat kojim ćemo omogućiti alat za provjeru dezinformacija, na HKS-u ćemo pokrenuti izborni kolegij posvećen samo medijima i dezinformacijama, izradit ćemo i sveučilišni udžbenik i mnogo vremena posvetiti edukacijama stručnih suradnika u tiskovnim uredima diljem Hrvatske, kao i studentima i učenicima koji moraju znati prepoznati dezinformacije u vjerskom prostoru“, objasnila je doc. dr. sc. Lana Ciboci Perša za IKA-u.
+Na projektu će sudjelovati stručnjaci i znanstvenici iz područja komunikologije i novinarstva, a unutar partnerskih institucija bit će pokrenute posebne jedinice: tako će na HKR-u biti pokrenuta zasebna redakcijska jedinica koja će se baviti provjerom točnosti informacija o vjerskim temama, a na portalu Hrvatske katoličke mreže nova rubrika „Glas istine“.Ovaj projekt prvi je ovakve vrste koji će se baviti sustavnim provjeravanjem činjenica o vjerskim temama u hrvatskom medijskom prostoru.Nositelji mjere koja se provodi u okviru Nacionalnog plana oporavka i otpornosti su Ministarstvo kulture i medija i Agencija za elektroničke medije.
+.Izradio Odjel za komunikologiju, HKS.")
     )
   ),
-  hr(), # Horizontal line for better separation of content
-  h4("Rezultati analize:"),
-  uiOutput("result"),
 
   # Footer
   tags$footer(
     style = "position: fixed; bottom: 0; width: 100%; background-color: #f5f5f5; text-align: center; padding: 7px;",
     "Built with ❤ by lux: © 2023 Projekt u suradnji HKS, HKR i HDKN za uspostavu sustava provjere točnosti informacija.“ | ",
-    a(href = "https://www.unicath.hr/projekt-kriticka-analiza-dezinformacija-o-vjerskim-temama-hks-a-i-partnerskih-institucija-dobio-bespovratna-sredstva-za-uspostavu-sustava-provjere-tocnosti-informacija"," Web")
+    a(href = "https://www.unicath.hr/projekt-kriticka-analiza-dezinformacija-o-vjerskim-temama-hks-a-i-partnerskih-institucija-dobio-bespovratna-sredstva-za-uspostavu-sustava-provjere-tocnosti-informacija", "Web")
   )
 )
 
@@ -51,6 +98,22 @@ server <- function(input, output) {
     }
     return(NULL)
   }
+
+
+  # Reactive value for text length
+  text_length <- reactive({
+    nchar(input$text)
+  })
+
+  # Reactive output for text length feedback
+  output$text_feedback <- renderText({
+    if (text_length() > 0 && text_length() < 50) {
+      "Tekst je prekratak za analizu. Unesite duži tekst."
+    } else {
+      NULL
+    }
+  })
+
 
   observeEvent(input$check, {
     if (input$text != "") {
